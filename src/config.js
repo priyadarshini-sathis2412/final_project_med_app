214d9dad45a153c943c520a4d211cd22bc689234
priyadarshini-sathis2412 <darshini.sathis@gmail.com>
2024-09-20 12:14:32 -0400

    data base connection

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_URL = window.location.hostname === "localhost" ? "https://darshinisath-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" :"https://darshinisath-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" 
