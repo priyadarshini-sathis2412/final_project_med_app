6f03ce9e16628fdf7eba566a8a22cc4a963096dd
priyadarshini-sathis2412 <darshini.sathis@gmail.com>
2024-10-01 01:18:19 -0400

    launch app

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://darshinisath-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" :"https://darshinisath-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" 
+// export const API_URL = window.location.hostname === "localhost" ? "https://darshinisath-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" :"https://darshinisath-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" 
+
+export const API_URL = "final_project_med_app"
